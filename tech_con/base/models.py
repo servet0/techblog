fd3618dc0980d8fb0d161f1b40dc959bc4bc6ee2
Servet Acar <servetacar540@gmail.com>
2023-09-13 22:58:51 +0300

    models and css

--- tech_con/base/models.py
+++ tech_con/base/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Logo(models.Model):
-    tittle = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='logo')
+    tittle = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to='logo', null=True, blank=True)
+    minilogo = models.ImageField(upload_to='minilogo', null=True, blank=True)
+    name = models.CharField(max_length=500, null=True, blank=True)
+
+
+
 
 # Create your models here.
